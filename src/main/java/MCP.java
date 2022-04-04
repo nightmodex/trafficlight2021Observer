dcbb802880cb076d55215349ef8ed3d897f71eb4
nightmodex <thomas.noori@stud.fh-campuswien.ac.at>
2022-04-04 19:36:45 +0200

    second commit

--- src/main/java/MCP.java
+++ src/main/java/MCP.java
@@ -1,9 +1,11 @@
 import trafficlight.ctrl.TrafficLightCtrl;
 
+//https://github.com/nightmodex/trafficlight2021Observer.git
 
 public class MCP {
     public static void main(String[] args) {
         TrafficLightCtrl ctrl = TrafficLightCtrl.getInstance();
+
         ctrl.run();
     }
 }
